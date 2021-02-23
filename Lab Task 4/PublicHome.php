2d17896e8fbe7c931b5fbfb59cb647fea2db503e
Towsif-Refat <you@example.com>
2021-02-24 00:00:06 +0600

    Lab Task 4 Submission

---
+++ Lab Task 4/PublicHome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Public Home Page</title>
+	
+</head>
+<body>
+<?php include('Header.php');?>
+<table>
+	<h2 style="margin-left: 2%;">Welcome To School Management Syatem</h>
+<tr>	
+<td style="padding: 100%">
+</td>
+<td style="padding: 100%">
+</td>
+<td style="padding: 65%">
+</td>
+</tr>
+</table>
+
+
+<?php include('Footer.php');?>
+
+</body>
+</html>
